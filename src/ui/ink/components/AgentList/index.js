03e2aa088ff038bd8de2b4ea3dd719fa13d28e33
Patrick Bassut <patrickbassut@hotmail.com>
2025-07-19 23:56:25 -0300

    fix: add JS wrapper for AgentList components
    - Created index.js wrapper to export AgentListCompat
    - Fixes TSX import error when starting Napoleon
    - Enables proper module loading for Ink UI components

---
+++ src/ui/ink/components/AgentList/index.js
@@ -0,0 +1,8 @@
+// JavaScript wrapper for AgentList components
+// Use the compatible version instead of TSX
+
+const { AgentListCompat } = require('./AgentListCompat');
+
+module.exports = {
+  AgentList: AgentListCompat,
+};
