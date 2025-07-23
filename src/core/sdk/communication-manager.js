6c5a8885d431459b4bb4da9454a2bf7decf1a356
Patrick Bassut <patrickbassut@hotmail.com>
2025-07-23 01:59:57 -0300

    remove bun

--- src/core/sdk/communication-manager.js
+++ src/core/sdk/communication-manager.js
@@ -153,7 +153,6 @@ class SDKCommunicationManager {
       // Merge options with session defaults
       const queryOptions = {
         permissionMode: 'bypassPermissions',
-        executable: 'bun',
         ...session.options,
         ...options,
         abortController: session.abortController,
